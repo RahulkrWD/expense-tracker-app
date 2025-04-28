58560771fa4a16138ddf1603a0d6096493add571
RahulkrWD <rahulkmrgaya21@gmail.com>
2025-04-28 13:56:36 +0530

    code push

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useSelector } from "react-redux";
 
 function Statistics() {
+  const data = useSelector((state) => state.expense.data);
+  let obj = {};
+  data.map((item) => {
+    if (!obj[item.category]) {
+      obj[item.category] = [];
+    }
+    obj[item.category].push(item);
+    return obj;
+  });
   return (
     <Layout>
-      <h3>Statistics</h3>
+      <div>
+        <h3>Statistics</h3>
+
+        {data.length == 0 ? (
+          <div>
+            <h3 className="text-center">No Expenses Found</h3>
+          </div>
+        ) : (
+          <div>
+            <div>
+              {obj.credit.map((item) => (
+                <div key={item.id}></div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </Layout>
   );
 }
